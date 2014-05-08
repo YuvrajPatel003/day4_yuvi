3acaceb7adafd14131bc2a61323175c1f7c3756f
YuvrajPatel003 <user1@Administrators-iMac-85.local>
2014-05-08 14:41:02 -0400

    finish lab work

---
+++ keyboard_input/keyboard_input/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  keyboard_input
+//
+//  Created by User1 on 2014-05-08.
+//  Copyright (c) 2014 Objective. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int n, number,triangularnumber;
+        NSLog(@"what triangular number do you want");
+        scanf("%i", &number);
+        triangularnumber=0;
+        
+        for(n=1;n<=number; ++n)
+        {
+            triangularnumber +=n;
+        }
+        NSLog(@"triangular number %i is %i\n",number,triangularnumber);
+        
+        
+    }
+    return 0;
+}
+
---
+++ table_of_triangular_no/table_of_triangular_no/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  table_of_triangular_no
+//
+//  Created by User1 on 2014-05-08.
+//  Copyright (c) 2014 Objective. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int n, triangularnumber;
+        NSLog(@"\nTABLE OF TRIANGULAR NUMBERS");
+        NSLog(@"\n sum of 1 to n");
+        NSLog(@"-- ---------");
+        triangularnumber = 0;
+        
+        for(n=1;n<=10; ++n)
+        {
+            triangularnumber += n;
+            NSLog(@" %i     %i", n,triangularnumber);        }
+    }
+    return 0;
+}
+
